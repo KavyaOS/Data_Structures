4043ab6421a6090b5b762ee13b7e254a09b5c0ed
Kavya O S <kavya.os15@gmail.com>
2021-07-02 19:20:14 -0700

    Syntax modified

--- Linked_List/Singly_Linked_List.py
+++ Linked_List/Singly_Linked_List.py
@@ -7,6 +7,12 @@ class Singly_linked_list:
     def __init__(self):
         self.head_value = None
 
+    def print_list(self):
+        node = self.head_value
+        while node is not None:
+            print(node.data_value)
+            node = node.nextval
+
 list1 = Singly_linked_list()
 list1.head_value = Node("Mon")
 element2 = Node("Tue")
@@ -16,4 +22,6 @@ element3 = Node("Wed")
 list1.head_value.nextval = element2
 
 #Link element2 to list1
-list1.head_value.nextval = element3
+element2.nextval = element3
+
+list1.print_list()
